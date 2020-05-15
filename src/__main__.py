b5fd0093ef7d17328ed565e4df035de511d696ed
fswt <fabian.stolp@student.hpi.uni-potsdam.de>
2020-05-15 08:29:51 +0200

    divide calculate_auto_correlation_coefficients into two functions

--- src/__main__.py
+++ src/__main__.py
@@ -11,22 +11,26 @@ def autocorrelation_phyphox():
     data_frame = preprocessing.align_data(data_frame, listening_rate=20)
 
     # calculate auto-correlation coefficients for acceleration
-    data_frame_acceleration, mean_coefficient_data_frame_acceleration = calculate_auto_correlation_coefficients(data_frame, "acceleration")
+    data_frame_acceleration = get_data_frame_from_sensor_data(data_frame, "acceleration")
+    mean_coefficient_data_frame_acceleration = calculate_auto_correlation_coefficients(data_frame_acceleration)
 
     # calculate auto-correlation coefficients for gyroscope
-    data_frame_gyroscope, mean_coefficient_data_frame_gyroscope = calculate_auto_correlation_coefficients(data_frame, "gyroscope")
+    data_frame_gyroscope = get_data_frame_from_sensor_data(data_frame, "gyroscope")
+    mean_coefficient_data_frame_gyroscope = calculate_auto_correlation_coefficients(data_frame_gyroscope)
 
     data_frames = [data_frame_acceleration, data_frame_gyroscope, mean_coefficient_data_frame_acceleration,
                    mean_coefficient_data_frame_gyroscope]
     visualization.visualize_different_sensors(data_frames, number_of_plot_rows=2, number_of_plot_cols=2, save=True)
 
-
-def calculate_auto_correlation_coefficients(data_frame, sensor_name):
+def get_data_frame_from_sensor_data(data_frame, sensor_name):
     _regex = "{sensor_name}_{dimension}".format(sensor_name=sensor_name, dimension=shared_constants.DIMENSIONS_KEY_LIST)
     data_frame = data_frame.filter(regex=_regex, axis=1)
+    return data_frame
+
+def calculate_auto_correlation_coefficients(data_frame):
     coefficient_data_frame = calculate_auto_correlation_data_frame(data_frame)
     mean_coefficient_data_frame = coefficient_data_frame.apply(np.mean, axis=1).to_frame()
-    return data_frame, mean_coefficient_data_frame
+    return mean_coefficient_data_frame
 
 
 if __name__ == "__main__":
