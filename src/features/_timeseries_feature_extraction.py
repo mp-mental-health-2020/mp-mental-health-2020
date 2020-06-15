e01cd5bf154ac662526aac4e57987d38f12c69e4
Julius Rudolph <julius.rudolph@student.hpi.de>
2020-06-15 14:00:05 +0200

    fix annotations

--- src/features/_timeseries_feature_extraction.py
+++ src/features/_timeseries_feature_extraction.py
@@ -1,6 +1,6 @@
 from tsfresh import extract_features
 
 
-def extract_timeseries_features(timeseries, y):
+def extract_timeseries_features(timeseries):
     features = extract_features(timeseries, column_id='action_id')
     return features
