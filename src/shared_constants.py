f15b4f6fa69364520c27a39b050f89675d69513d
Julius Rudolph <julius.rudolph@student.hpi.de>
2020-07-27 12:08:04 +0200

    add sliding window segmentation with overlap

--- src/shared_constants.py
+++ src/shared_constants.py
@@ -6,3 +6,5 @@ Constants shard between multiple files to stay consistent.
 DIMENSIONS_KEY_LIST = ["x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8]
 TIMESTAMP_KEY = "timestamp"
 ACTION_ID_COL = "action_id"
+SEGMENTATION_NO_OVERLAP = "segmentation_no_window"
+SEGMENTATION_OVERLAP = "segmentation_window"
